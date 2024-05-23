bcdb7533811621d752dd0edc6372db09c3741397
Zan-Kir <murilo4santos@hotmail.com>
2024-05-22 21:46:47 -0300

    fontawesome

--- Lumina2/src/app/app.component.ts
+++ Lumina2/src/app/app.component.ts
@@ -1,16 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MaterialModule } from './material.module';
-
-
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faCoffee } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MaterialModule],
+  imports: [RouterOutlet, MaterialModule, FontAwesomeModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
+
 export class AppComponent {
-  title = 'Lumina2';
+  title = 'Lumina2'; faCoffee = faCoffee;
 }
