688298f84db141188652a89ef01f1c17be23d059
Zan-Kir <murilo4santos@hotmail.com>
2024-05-23 14:18:37 -0300

    iconesfuncionais

--- Lumina2/src/app/app.component.ts
+++ Lumina2/src/app/app.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MaterialModule } from './material.module';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { faCoffee } from '@fortawesome/free-solid-svg-icons';
+
+
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -12,5 +13,5 @@ import { faCoffee } from '@fortawesome/free-solid-svg-icons';
 })
 
 export class AppComponent {
-  title = 'Lumina2'; faCoffee = faCoffee;
+  title = 'Lumina2';
 }
--- Lumina2/src/app/footer/footer.component.ts
+++ Lumina2/src/app/footer/footer.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { MaterialModule } from '../material.module';
+import { faWhatsapp, fab } from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
 export class FooterComponent {
-
+  fab = fab;
+  faWhatsapp = faWhatsapp
 }
