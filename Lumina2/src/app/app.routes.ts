797903ebd79673491980f695edced63244fd779c
Victor Favretto <vf.favretto@gmail.com>
2024-05-23 22:17:59 -0300

    routes funcionando

--- Lumina2/src/app/app.routes.ts
+++ Lumina2/src/app/app.routes.ts
@@ -4,6 +4,8 @@ import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
-     { path: 'home', component: AppComponent },
-    {path: 'login', component: LoginComponent},
+     { path: '', component: HomeComponent },
+     { path: 'home', component: HomeComponent },
+     {path: 'login', component: LoginComponent},
+     { path: '**', component: HomeComponent },
 ];
