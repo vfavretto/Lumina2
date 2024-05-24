a0ec6d11b4c01c2361492cae2bb1105291567bdc
Victor Favretto <vf.favretto@gmail.com>
2024-05-23 22:00:00 -0300

    login + div no app.component

--- Lumina2/src/app/app.component.ts
+++ Lumina2/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { MaterialModule } from './material.module';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
@@ -7,11 +7,12 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MaterialModule, FontAwesomeModule],
+  imports: [RouterOutlet, MaterialModule, FontAwesomeModule, RouterLink, RouterLinkActive],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 
 export class AppComponent {
   title = 'Lumina2';
+  
 }
--- Lumina2/src/app/app.routes.ts
+++ Lumina2/src/app/app.routes.ts
@@ -4,7 +4,6 @@ import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
-    // { path: 'home', component: AppComponent },
-    { path: 'home', component: HomeComponent },
+     { path: 'home', component: AppComponent },
     {path: 'login', component: LoginComponent},
 ];
